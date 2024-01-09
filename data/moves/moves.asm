210e4a893d2a2091261ada3920eb6fc445f3d444
JoururiChan <155913924+JoururiChan@users.noreply.github.com>
2024-01-08 20:01:49 -0500

    Update moves.asm

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -136,7 +136,7 @@ endc
 	move CONFUSION,    EFFECT_CONFUSE_HIT,        50, PSYCHIC,   100, 25,  10, SPECIAL
 	move PSYCHIC_M,    EFFECT_SP_DEF_DOWN_HIT,    90, PSYCHIC,   100, 10,  10, SPECIAL
 	move HYPNOSIS,     EFFECT_SLEEP,               0, PSYCHIC,    60, 20,   0, STATUS
-	move HONE_CLAWS,   EFFECT_HONE_CLAWS,          0, DARK,       -1, 40,   0, STATUS
+	move HONE_TOOLS,   EFFECT_HONE_CLAWS,          0, STEEL,      -1, 40,   0, STATUS
 	move AGILITY,      EFFECT_SPEED_UP_2,          0, PSYCHIC,    -1, 30,   0, STATUS
 	move QUICK_ATTACK, EFFECT_PRIORITY_HIT,       40, NORMAL,    100, 30,   0, PHYSICAL
 	move RAGE,         EFFECT_RAGE,               20, NORMAL,    100, 20,   0, PHYSICAL
