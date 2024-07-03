6290857de3a601a74c7b88cc2f50bf022e812fa5
JoururiChan <155913924+JoururiChan@users.noreply.github.com>
2024-07-03 10:12:43 -0400

    Revert "tesuto?"
    This reverts commit 271d18274a77b6d9b47be7b31c7e6a928000b111.

--- main.asm
+++ main.asm
@@ -487,13 +487,6 @@ INCLUDE "data/battle_anims/framesets.asm"
 INCLUDE "data/battle_anims/oam.asm"
 INCLUDE "data/battle_anims/object_gfx.asm"
 
-SECTION "bank34", ROMX
-
-SECTION "bank35", ROMX
-
-SECTION "bank36", ROMX
-
-SECTION "bank37", ROMX
 
 SECTION "Pic Animations", ROMX
 
