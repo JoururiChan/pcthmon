d02baf407717c3548a76db82a43b158a670cca27
JoururiChan <155913924+JoururiChan@users.noreply.github.com>
2024-09-29 12:23:33 -0400

    I wonder..

--- ram/wramx.asm
+++ ram/wramx.asm
@@ -1612,7 +1612,7 @@ ALIGN 8
 wDexAreaMons::
 ; Array size needs to be a multiple of 10 covering all landmarks for a region.
 ; Upper cap is 120.
-for n, 1, 100
+for n, 1, 90
 wDexAreaMon{d:n}::
 wDexAreaMon{d:n}YCoord:: db
 wDexAreaMon{d:n}XCoord:: db
